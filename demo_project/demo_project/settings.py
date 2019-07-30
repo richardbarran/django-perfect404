6b9197e8df4b84b41a4679e16ed95728a91ad97c
Richard Barran <richard@arbee-design.co.uk>
2019-07-30 18:49:04 +0200

    General cleanup of code.

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -25,7 +25,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -57,21 +56,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 LANGUAGE_CODE = 'en-us'
 
--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-perfect404',
-    version = __import__('perfect404').__version__,
-    description = 'Simple but perfect 404 page for django projects.',
-    keywords = 'django apps webdev',
-    license = 'New BSD License',
-    author = 'Alexander Artemenko',
-    author_email = 'svetlyak.40wt@gmail.com',
-    url = 'http://github.com/svetlyak40wt/django-perfect404/',
+    name='django-perfect404',
+    version=__import__('perfect404').__version__,
+    description='Simple but perfect 404 page for django projects.',
+    keywords='django apps webdev',
+    license='New BSD License',
+    author='Alexander Artemenko',
+    author_email='svetlyak.40wt@gmail.com',
+    url='http://github.com/richardbarran/django-perfect404/',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Plugins',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    packages = find_packages(exclude=['demo_project']),
-    package_data = {
+    packages=find_packages(exclude=['demo_project']),
+    package_data={
         'templates': ['*.html'],
     },
-    include_package_data = True,
-    zip_safe = True,
+    include_package_data=True,
+    zip_safe=True,
 )
 
