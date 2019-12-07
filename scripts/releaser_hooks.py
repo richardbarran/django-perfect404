670db5bd5b3cf3858399953b0715762ce81d0951
Richard Barran <richard@arbee-design.co.uk>
2019-12-07 21:24:00 +0100

    Fixed release script.

--- scripts/releaser_hooks.py
+++ scripts/releaser_hooks.py
@@ -3,7 +3,7 @@ import subprocess
 
 def prereleaser_middle(data):
     print('Running unit tests one last time before we make a release.')
-    subprocess.check_output(["python", "demo_project/manage.py", "test"])
+    subprocess.check_output(["python", "demo_project/manage.py", "test", "demo_project"])
 
     print('Running flake8 check (PEP8 conventions).')
     # See setup.cfg for configuration options.
