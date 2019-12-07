49ec6f7f9e2ee7ff3c73e9699db46241de181b14
Richard Barran <richard@arbee-design.co.uk>
2019-12-07 21:20:41 +0100

    Replace pycodestyle with flake8.

--- scripts/releaser_hooks.py
+++ scripts/releaser_hooks.py
@@ -5,6 +5,6 @@ def prereleaser_middle(data):
     print('Running unit tests one last time before we make a release.')
     subprocess.check_output(["python", "demo_project/manage.py", "test"])
 
-    print('Running pycodestyle check (PEP8 conventions).')
+    print('Running flake8 check (PEP8 conventions).')
     # See setup.cfg for configuration options.
-    subprocess.check_output(["pycodestyle"])
+    subprocess.check_output(["flake8"])
