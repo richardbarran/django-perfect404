5c0cdc2fd3471ab4ed3fbf4454dd31a475a252fc
Richard Barran <richard@arbee-design.co.uk>
2021-04-29 19:46:06 +0200

    Checked compatibility with Django 3.2 and fixed deprecation warnings.

--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="home.html")),
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="home.html")),
 ]
 
 handler404 = 'perfect404.views.page_not_found'
